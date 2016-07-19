f04e11e7d8272e39e887a1e304679395edf30fe2
Nakzz <naqibprio10@gmail.com>
2016-07-19 11:52:26 -0400

    Local Files
    First Push

---
+++ src/trash/scrap.java
@@ -0,0 +1,7 @@
+package trash;
+public class scrap {
+  public static void main(String[] args) {
+       System.out.println("Working Directory = " +
+              System.getProperty("user.dir"));
+  }
+}
